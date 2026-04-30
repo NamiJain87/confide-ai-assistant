9b65176362b7f6171136a8d532e5d495cb9a947b
NAMIJAIN87 <namijain1510@gmail.com>
2026-05-01 01:09:28 +0530

    Fix React crash by importing useEffect in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useChat } from "./hooks/useChat";
 import ChatWindow from "./components/ChatWindow";
 import InputBar from "./components/InputBar";
